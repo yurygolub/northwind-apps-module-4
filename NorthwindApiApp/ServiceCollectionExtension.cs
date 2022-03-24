294c128d583d82f900788ce98ea73543579b2659
Yury Golub <yuragolub21@gmail.com>
2022-03-24 16:47:56 +0300

    Add run modes.

---
+++ NorthwindApiApp/ServiceCollectionExtension.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Northwind.Services;
+using Northwind.Services.Employees;
+using Northwind.Services.Products;
+using Northwind.Services.SqlServer;
+using DataAccess = Northwind.Services.DataAccess;
+using EntityFramework = Northwind.Services.EntityFrameworkCore;
+
+namespace NorthwindApiApp
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddSqlServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services
+                .AddTransient<IProductManagementService, DataAccess.Products.ProductManagementService>()
+                .AddTransient<IProductCategoryManagementService, DataAccess.Products.ProductCategoryManagementService>()
+                .AddTransient<IProductCategoryPicturesService, DataAccess.Products.ProductCategoryPicturesService>()
+                .AddTransient<IEmployeeManagementService, DataAccess.Employees.EmployeeManagementService>()
+                .AddTransient<IEmployeePicturesService, DataAccess.Employees.EmployeePicturesService>()
+                .AddScoped(s => new SqlConnection(configuration.GetConnectionString("SqlConnection")))
+                .AddTransient<NorthwindDataAccessFactory, SqlServerDataAccessFactory>();
+        }
+
+        public static IServiceCollection AddEfServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services
+                .AddTransient<IProductManagementService, EntityFramework.Products.ProductManagementService>()
+                .AddTransient<IProductCategoryManagementService, EntityFramework.Products.ProductCategoryManagementService>()
+                .AddTransient<IProductCategoryPicturesService, EntityFramework.Products.ProductCategoryPicturesService>()
+                .AddTransient<IEmployeeManagementService, EntityFramework.Employees.EmployeeManagementService>()
+                .AddScoped(s => configuration.GetConnectionString("SqlConnection"));
+        }
+    }
+}
--- NorthwindApiApp/Startup.cs
+++ NorthwindApiApp/Startup.cs
@@ -1,15 +1,8 @@
-﻿using System.Data.SqlClient;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Northwind.Services;
-using Northwind.Services.Employees;
-using Northwind.Services.Products;
-using Northwind.Services.SqlServer;
-using DataAccess = Northwind.Services.DataAccess;
-using EntityFramework = Northwind.Services.EntityFrameworkCore;
 
 #pragma warning disable SA1600
 
@@ -41,21 +34,18 @@ namespace NorthwindApiApp
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-                .AddTransient<IProductManagementService, DataAccess.Products.ProductManagementService>()
-                .AddTransient<IProductCategoryManagementService, DataAccess.Products.ProductCategoryManagementService>()
-                .AddTransient<IEmployeeManagementService, DataAccess.Employees.EmployeeManagementService>()
-                .AddScoped(s => this.Configuration.GetConnectionString("SqlConnection"))
-                .AddTransient<IProductManagementService, EntityFramework.Products.ProductManagementService>()
-                .AddTransient<IProductCategoryManagementService, EntityFramework.Products.ProductCategoryManagementService>()
-                .AddTransient<IEmployeeManagementService, EntityFramework.Employees.EmployeeManagementService>()
-                .AddScoped(s =>
-                {
-                    string connecionString = this.Configuration.GetConnectionString("SqlConnection");
-                    return new SqlConnection(connecionString);
-                })
-                .AddTransient<NorthwindDataAccessFactory, SqlServerDataAccessFactory>()
-                .AddControllers();
+            switch (this.Configuration["Mode"])
+            {
+                case "Sql":
+                    services.AddSqlServices(this.Configuration);
+                    break;
+
+                case "Ef":
+                    services.AddEfServices(this.Configuration);
+                    break;
+            }
+
+            services.AddControllers();
         }
     }
 }
